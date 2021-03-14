dd0885f634818277f4ca128fb056902734ee8155
Kal Sajana Samarasinghe <sajanasamarasinghe@gmail.com>
2021-03-14 00:26:42 -0700

    1) Rename the script. 2) Added export to JSON and also RDS 3) Changed the names of the output files, and organized the output

--- ExportData.R
+++ ExportData.R
@@ -2,15 +2,17 @@ library(tidyverse)
 library(magrittr)
 library(glue)
 library(janitor)
+library(jsonlite)
 
-# Import -------------
+# Import & Create required directories ---------------------------------------------
 rm(list = ls())
 nestdb <- readRDS("rdsobj/nestdb.RDS")
 nestdb_md5 <- tools::md5sum("rdsobj/nestdb.RDS")
 
-coln <- read.csv('reference/col_names.csv', header = FALSE)
+## Create folder for outputs if it does not exist
+dir.create("output")
 
-# Other edits -------------------
+# Universal edits ---------------------------------------------------------------
 tempdb <- nestdb 
 
 ## Remove TURPS from the biopsies
@@ -21,13 +23,13 @@ tempdb %<>%
 
 ## Remove the names of persons who had a family hx
 tempdb %<>% 
-  mutate(posFamHx = map_lgl(data.fhx,~ .x %>% janitor::remove_empty("rows") %>% {ifelse(nrow(.) == 0,FALSE,TRUE)})) %>% 
+  mutate(posFamHx = map_lgl(data.fhx,~ .x %>% janitor::remove_empty("rows") %>% {ifelse(nrow(.) == 0,FALSE,TRUE)}),.before = "LastFU") %>% 
   select(!data.fhx)
 
 # Remove LastFUOrig & dat.oth
 tempdb %<>% select(!c("LastFU.Orig","data.oth"))
 
-# WIDENING----------------
+# WIDE CSV-----------------------------------------------------------------------
 tempdb_wide <- tempdb
 
 ## Unnest Treatment stat
@@ -56,10 +58,9 @@ tempdb_wide %<>%
   group_by(ID) %>% 
   pivot_wider(names_from = ID.Biop, values_from = Cases)
 
-widedb <- tempdb_wide; rm(tempdb_wide)
-write_csv(widedb, file = "output/widetbl.csv")
+write_csv(tempdb_wide, file = "output/DB_Wide.csv")
 
-# SEP_Tables grouped by ID ---------------------
+# SEPERATE CSV ---------------------------------------------------------------------
 patInfo <- tempdb %>% 
   select(!c(data.biop,data.trt))
 
@@ -71,7 +72,14 @@ biopsyInfo <- tempdb %>%
   select(c(ID,data.biop)) %>% 
   unnest(data.biop, keep_empty = TRUE) 
 
-dir.create("output/nesteddf")
-write_csv(patInfo, file = "output/nesteddf/patInfo.csv")
-write_csv(trtInfo, file = "output/nesteddf/trtInfo.csv")
-write_csv(biopsyInfo, file = "output/nesteddf/biopsyInfo.csv")
+dir.create("output/DB_Split")
+write_csv(patInfo, file = "output/DB_Split/DBSplit_PatInfo.csv")
+write_csv(trtInfo, file = "output/DB_Split/DBSplit_TrtInfo.csv")
+write_csv(biopsyInfo, file = "output/DB_Split/DBSplit_BiopsyInfo.csv")
+
+# JSON -----------------------------------------------------------------------------
+exportJSON <-toJSON(tempdb,pretty = TRUE)
+write(exportJSON,"output/DB_Nested.json")
+
+#RDS --------------------------------------------------------------------------------
+write_rds(tempdb,"output/DB_Nested.rds")
